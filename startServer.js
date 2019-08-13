2ca8c548f394e5624432c08b0c6b5c0893fafde1
Jak Rowan <jak@rowan.li>
2019-08-13 17:20:35 -0400

    Create server plugin

---
+++ startServer.js
@@ -0,0 +1,39 @@
+/* eslint-disable no-console,global-require */
+
+const Hapi = require('@hapi/hapi');
+const path = require('path');
+
+require('dotenv').config({
+  path: path.join(__dirname, '.env'),
+  silent: true,
+});
+
+startServer()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  });
+
+async function startServer() {
+  const server = Hapi.server({
+    address: process.env.ADDRESS,
+    port: process.env.PORT,
+  });
+
+  const plugins = [{
+    plugin: require('@hapi/good'),
+    options: {
+      reporters: {
+        consoleReporter: [{
+          module: '@hapi/good-console',
+        }, 'stdout'],
+      },
+    },
+  }, {
+    plugin: require('./src'),
+    options: {},
+  }];
+
+  await server.register(plugins);
+  await server.start();
+}
